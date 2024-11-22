b993bb359b64835e5f6a060a200c487795569016
Sophia Nguyen <nguyencsg@gmail.com>
2024-11-21 19:50:59 -0600

    first commit. initial file

---
+++ hangman.py
@@ -0,0 +1,6 @@
+word_list = ["aardvark", "baboon", "camel"]
+# Randomly choose a word from the word_list and assign it to a variable called chosen_word. Print it
+
+# Ask the user to guess a letter and assign their answer to variable called guess. Make the String stored in guess lowercase.
+
+# Check if the letter the user guessed guess is one of the letters in the chosen_word. Loop through each of the letters in the chosen_word and print "Right" if the letter is a match, "Wrong" if it's not
